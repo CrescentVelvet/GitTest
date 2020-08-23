28af93248baa8b77420ba45b6ee500deae80b8ee
CrescentVelvet <crescentvelvet3@gmail.com>
2020-08-23 19:31:27 +0800

    完善六月时间表记录

--- Schedule/schedule.py
+++ Schedule/schedule.py
@@ -11,7 +11,7 @@ def velvet():
 	wyf_type=[]
 	size=27
 	data=[]
-	with open("schedule.txt", 'r') as f: # 这样不用f.close()了
+	with open("2020.06-schedule.txt", 'r') as f: # 这样不用f.close()了
 		line=f.readline()
 		while line:
 			line = f.readline()
@@ -38,16 +38,18 @@ def velvet():
 
 	# 绘制图像
 	# plt.bar(x,height,width,bottom)
+	# 绘制间隔设为delay
+	delay = 9
 	for index in range(len(data)):
-		# 划分子图
-		n = int(wyf_day[index] // 10 + 1)
+		# 划分子图，1-9日在1图，10-17日在2图,18-26日在3图,27-31日在4图
+		n = int((wyf_day[index]-1) // delay + 1)
 		plt.subplot(2,2,n)
 		# 绘制直方图
 		plt.bar(wyf_day[index],wyf_t2[index],bottom=wyf_t1[index],fc=wyf_type[index])
-	# 给子图定坐标
-	for n in range(int(wyf_day[-1] // 10 + 1)):
+	# 给子图定坐标，x坐标0-10,9-18,17-25,26-32如此分布
+	for n in range((int(wyf_day[-1]-1) // delay + 1)):
 		plt.subplot(2,2,n+1)
-		plt.xlim(10*n-1,10*(n+1))
+		plt.xlim(delay*n,delay*n+delay+1)
 		plt.ylim(0,24)
 		plt.gca().invert_yaxis() # 坐标反向
 		plt.legend()
@@ -62,11 +64,7 @@ def velvet():
 	plt.bar(0,0,label='bili',fc='pink')
 	plt.bar(0,0,label='game',fc='lightsalmon')
 	plt.bar(0,0,label='June',fc='black')
-	plt.xlim(30,40)
-	plt.ylim(0,24)
-	plt.gca().invert_yaxis() # 坐标反向
 	plt.legend()
-	plt.grid(True)
 
 if __name__=="__main__":
 	velvet()
