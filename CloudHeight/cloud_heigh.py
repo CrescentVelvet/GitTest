3628b548598a6a2e8ccf11fdf58a995e8bdb637a
CrescentVelvet <crescentvelvet3@gmail.com>
2021-01-10 21:07:58 +0800

    放弃鱼眼图像畸变校正

--- CloudHeight/cloud_heigh.py
+++ CloudHeight/cloud_heigh.py
@@ -1,7 +1,7 @@
 '''
 Author       : velvet
 Date         : 2021-01-09 15:19:57
-LastEditTime : 2021-01-10 21:02:50
+LastEditTime : 2021-01-10 21:07:12
 LastEditors  : velvet
 Description  : 双目视觉云高估计
 '''
@@ -48,36 +48,11 @@ image_nb_gray = cv2.imread('nb-20201119-1212.png', cv2.IMREAD_GRAYSCALE)
 # image_sc_circle = cut_circle(image_sc)
 # image_nb_circle = cut_circle(image_nb)
 
-# # 图像畸变校正
-# # OpenCV像素坐标系的原点在图像左上角
-# # 截图的图片为905*901，不是正方形
-# center_x = (image_nb.shape[:2][0] - 5) // 2
-# center_y = (image_nb.shape[:2][1] - 1) // 2
-# center_radius = int(center_x*math.sqrt(2)) + 1
-# center_radius_correct = int(center_radius*3.14159*0.5) + 1
-# # 新建纯黑图像
-# image_nb_correct = np.zeros([center_radius_correct*2, center_radius_correct*2, 3], np.uint8)
-# for y in range(center_y*2):
-#     for x in range(center_x*2):
-#         # 原图像的xy坐标转换极坐标
-#         radius = math.sqrt((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y))
-#         # 极坐标下的畸变校正
-#         radius_correct = center_radius * (math.asin(radius / center_radius))
-#         # 校正图像的极坐标转换xy坐标
-#         if radius == 0:
-#             x_correct = int(center_radius_correct / math.sqrt(2))
-#             y_correct = int(center_radius_correct / math.sqrt(2))
-#             print(x_correct)
-#         else:
-#             x_correct = int((x - center_x) * (radius_correct / radius) + center_radius_correct / math.sqrt(2))
-#             y_correct = int((y - center_y) * (radius_correct / radius) + center_radius_correct / math.sqrt(2))
-#         image_nb_correct[x_correct, y_correct, 0] = image_nb[x, y, 0]
-#         image_nb_correct[x_correct, y_correct, 1] = image_nb[x, y, 1]
-#         image_nb_correct[x_correct, y_correct, 2] = image_nb[x, y, 2]
 
 # 边缘检测
 # 对灰度图像添加高斯模糊
 gauss_nb_gray = cv2.GaussianBlur(image_nb_gray, (3, 3), 0)
+# canny边缘检测
 canny_nb_gray = cv2.Canny(gauss_nb_gray, 50, 150)
 
 
