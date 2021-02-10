a95e18156c9d4f78dcb3a43dd142713539892f89
CrescentVelvet <crescentvelvet3@gmail.com>
2021-02-11 00:26:09 +0800

    保存原神抽卡记录

--- RobotOld/GNN/tran_dataset.py
+++ RobotOld/GNN/tran_dataset.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 import pickle
-import read_csv
+import csv
 import os
 import torch
 from torch_geometric.data import Data
 
 print("1")
 print(np.random.seed(42))
-buy_df = pd.read_csv('../input/yoochoose-buys.dat', header=None)
+df = pd.read_csv('yoochoose-data/yoochoose-clicks.dat', header=None)
+
+buy_df = pd.read_csv('yoochoose-data/yoochoose-buys.dat', header=None)
 buy_df.columns=['session_id','timestamp','item_id','price','quantity']
 print(buy_df.head(20))
