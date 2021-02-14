996862f419d143afa484d2d8d862fd5e0c8c6467
CrescentVelvet <crescentvelvet3@gmail.com>
2021-02-14 17:29:17 +0800

    添加pandas函数注释

--- RobotOld/GNN/tran_dataset.py
+++ RobotOld/GNN/tran_dataset.py
@@ -7,16 +7,23 @@ import torch
 from torch_geometric.data import Data
 from sklearn.preprocessing import LabelEncoder
 
-# 数据读取
 df = pd.read_csv('yoochoose-data/yoochoose-clicks.dat', header=None)
 df.columns = ['session_id','timestamp','item_id','category']
 buy_df = pd.read_csv('yoochoose-data/yoochoose-buys.dat', header=None)
 buy_df.columns=['session_id','timestamp','item_id','price','quantity']
 # print(df.head(5)) # 输出前5个数据
 # print(buy_df.head(5))
+# map运算函数(函数，列表)：对列表中的每一个元素进行函数运算
+# lambda匿名函数 x：表示一个任意变量x
+# groupby分组函数(标签)：根据标签对数据分组，相同标签的数据放到一起
+# loc选取函数[行，列]：按照行和列的标签选取数据，取出某一行列的数据
+# drop删除函数(标签)：删除该标签行，列要加个axis=1
+# 整体语句的含义：合并df数据集中id相同的数据，对每一个合并尺寸判断与2的大小
+# 判断结果放进valid_session，删除该列
 df['valid_session'] = df.session_id.map(df.groupby('session_id')['item_id'].size() > 2)
 df = df.loc[df.valid_session].drop('valid_session',axis=1)
-# 二次采样
+# 二次采样减小数据量
+# 
 sampled_session_id = np.random.choice(df.session_id.unique(), 1000000, replace=False)
 df = df.loc[df.session_id.isin(sampled_session_id)]
 # print(df.nunique()) # 输出数据中各种类别的个数
@@ -24,4 +31,5 @@ df = df.loc[df.session_id.isin(sampled_session_id)]
 # print(df.isna().sum())
 # 检查click数据集中的session_id是否出现在buy数据集中
 df['label'] = df.session_id.isin(buy_df.session_id)
-print(df.head())
+
+dataset = dataset.shuffle()
