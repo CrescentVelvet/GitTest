fbb506b4b9c9469851357ebc567eb3d70beba982
CrescentVelvet <crescentvelvet3@gmail.com>
2021-02-18 17:59:03 +0800

    整理pandas注释

--- RobotOld/MYGNN/main_gnn.py
+++ RobotOld/MYGNN/main_gnn.py
@@ -1,7 +1,7 @@
 '''
 Author       : velvet
 Date         : 2021-02-17 23:04:22
-LastEditTime : 2021-02-18 15:14:16
+LastEditTime : 2021-02-18 17:37:36
 LastEditors  : velvet
 Description  : 
 '''
@@ -23,7 +23,7 @@ class MyDataset(InMemoryDataset):
     @property
     # 数据集保存文件夹
     def processed_file_names(self):
-        return ['./my_gnn_data.dataset'] # 保存在当前目录下
+        return ['../20210117/data/my_gnn_data.dataset'] # 保存在data目录下
     # 从加载文件夹里读取数据
     def download(self):
         pass
@@ -34,13 +34,23 @@ class MyDataset(InMemoryDataset):
         # grouped = df.groupby('session_id')
         # for session_id, group in tqdm(grouped):
         for i in df.index:
-            print(i)
+            # 判断数据缺失
+            if np.isnan(df.enemy7_y[i]):
+                print('Yes')
+            else:
+                pass
+            # print(len(df[i]))
+            # 边
             edge_index = torch.tensor([[0, 1, 1, 2],
                            [1, 0, 2, 1]], dtype=torch.long)
+            # 节点
             x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
-            y = torch.tensor([[1], [0], [-1]], dtype=torch.float)
+            # if np.isnan(df.me1_x[i]):
+            #   me1_x = torch.tensor([df.me1_x], dtype=torch.float)
+            # else:
+            #   pass
             # 节点和边组成数据
-            data = Data(x=x, edge_index=edge_index, y=y)
+            data = Data(x=x, edge_index=edge_index)
             # 数据添加到数据列表
             data_list.append(data)
         # 整理数据对象以增加读取速度，将所有实例连接到一个大数据对象中
@@ -52,21 +62,18 @@ class MyDataset(InMemoryDataset):
 df = pd.read_csv('data/2019-07-03_14-09_ER-Force-vs-TIGERs_Mannheim_data.txt',
     header=None, sep='\t', index_col=False,
     names=['ball_x','ball_y',
-    'me1_x','me1_y','me2_x','me2_y','me3_x','me3_y','me4_x',
-    'me4_y','me5_x','me5_y','me6_x','me6_y','me7_x','me7_y',
+    'me1_x','me1_y','me2_x','me2_y','me3_x','me3_y','me4_x','me4_y','me5_x','me5_y',
+    'me6_x','me6_y','me7_x','me7_y','me8_x','me8_y','me9_x','me9_y',
     'enemy1_x','enemy1_y','enemy2_x','enemy2_y','enemy3_x','enemy3_y','enemy4_x',
     'enemy4_y','enemy5_x','enemy5_y','enemy6_x','enemy6_y','enemy7_x','enemy7_y'])
 df = df.reset_index()
 df.columns=['index',
     'ball_x','ball_y',
-    'me1_x','me1_y','me2_x','me2_y','me3_x','me3_y','me4_x',
-    'me4_y','me5_x','me5_y','me6_x','me6_y','me7_x','me7_y',
+    'me1_x','me1_y','me2_x','me2_y','me3_x','me3_y','me4_x','me4_y','me5_x','me5_y',
+    'me6_x','me6_y','me7_x','me7_y','me8_x','me8_y','me9_x','me9_y',
     'enemy1_x','enemy1_y','enemy2_x','enemy2_y','enemy3_x','enemy3_y','enemy4_x',
     'enemy4_y','enemy5_x','enemy5_y','enemy6_x','enemy6_y','enemy7_x','enemy7_y']
-print(df)
-# buy_df = pd.read_csv('yoochoose-data/yoochoose-buys.dat', header=None)
-# buy_df.columns=['session_id','timestamp','item_id','price','quantity']
-# print(buy_df)
+# print(df)
 print("----------预处理完成----------")
 dataset = MyDataset(root='../')
 train_dataset = dataset[:2]
