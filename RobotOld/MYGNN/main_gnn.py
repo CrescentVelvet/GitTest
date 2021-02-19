2eb0efffd6b1c3b66076aa8180557b9065c2c9c8
CrescentVelvet <crescentvelvet3@gmail.com>
2021-02-20 02:02:19 +0800

    尝试自己建立层与网络模型

--- RobotOld/MYGNN/main_gnn.py
+++ RobotOld/MYGNN/main_gnn.py
@@ -1,20 +1,111 @@
 '''
 Author       : velvet
 Date         : 2021-02-17 23:04:22
-LastEditTime : 2021-02-19 16:35:04
+LastEditTime : 2021-02-20 02:01:13
 LastEditors  : velvet
 Description  : 
 '''
 import numpy as np
 import pandas as pd
 import torch
+from torch.nn import Sequential as Seq, Linear, ReLU
+from torch_geometric.nn import MessagePassing, GraphConv
+from torch_geometric.utils import add_self_loops, degree
 from torch_geometric.data import Data, InMemoryDataset, DataLoader
 from sklearn.preprocessing import LabelEncoder
 
-# 创建自己的数据集dataset
-class MyDataset(InMemoryDataset):
+# 创建GCN层
+class GCNConv(MessagePassing):
+    def __init__(self, in_channels, out_channels):
+        super(GCNConv, self).__init__(aggr='add')  # "Add" aggregation (Step 5).
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+    def forward(self, x, edge_index):
+        # x has shape [N, in_channels]
+        # edge_index has shape [2, E]
+        # Step 1: 向邻接矩阵添加自循环
+        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        # Step 2: 将节点特征矩阵线性变换
+        x = self.lin(x)
+        # Step 3: 计算标准化系数
+        row, col = edge_index
+        deg = degree(col, x.size(0), dtype=x.dtype)
+        deg_inv_sqrt = deg.pow(-0.5)
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+        # Step 4-5: 开始信息传递
+        return self.propagate(edge_index, x=x, norm=norm)
+    def message(self, x_j, norm):
+        # x_j has shape [E, out_channels]
+        # Step 4: 规范化节点特征
+        return norm.view(-1, 1) * x_j
+
+# 用SAGEConv层代替GraphConv层
+class SAGEConv(MessagePassing):
+    def __init__(self, in_channels, out_channels):
+        super(SAGEConv, self).__init__(aggr='max') # "Max" aggregation.
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+        self.act = torch.nn.ReLU()
+        self.update_lin = torch.nn.Linear(in_channels + out_channels, in_channels, bias=False)
+        self.update_act = torch.nn.ReLU()
+    def forward(self, x, edge_index):
+        # x的结构是[N, in_channels]
+        # edge_index的结构是[2, E]
+        edge_index, _ = remove_self_loops(edge_index)
+        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        return self.propagate(edge_index, size=(x.size(0), x.size(0)), x=x)
+    def message(self, x_j):
+        # x_j的结构是[E, in_channels]
+        x_j = self.lin(x_j)
+        x_j = self.act(x_j)
+        return x_j
+    def update(self, aggr_out, x):
+        # aggr_out的结构是[N, out_channels]
+        new_embedding = torch.cat([aggr_out, x], dim=1)
+        new_embedding = self.update_lin(new_embedding)
+        new_embedding = self.update_act(new_embedding)
+        return new_embedding
+
+# 创建自己的神经网络模型
+class RobotNet(torch.nn.Module):
+    # 模型结构初始化(隐层、激活函数、层结构等)
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 out_dim,
+                 n_layers,
+                 activation,
+                 dropout,
+                 aggregator_type):
+        # 调用父类的构造函数
+        super(RobotNet, self).__init__()
+        # 添加层
+        self.conv1 = SAGEConv(embed_dim, 128)
+    # 网络前向传递过程，训练、测试过程中使用model(g,features)直接执行
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.item_embedding(x)
+        x = x.squeeze(1)
+        x = F.relu(self.conv1(x, edge_index))
+        x, edge_index, _, batch, _ = self.pool1(x, edge_index, None, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+        x = F.relu(self.conv2(x, edge_index))
+        x, edge_index, _, batch, _ = self.pool2(x, edge_index, None, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+        x = F.relu(self.conv3(x, edge_index))
+        x, edge_index, _, batch, _ = self.pool3(x, edge_index, None, batch)
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+        x = x1 + x2 + x3
+        x = self.lin1(x)
+        x = self.act1(x)
+        x = self.lin2(x)
+        x = self.act2(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = torch.sigmoid(self.lin3(x)).squeeze(1)
+        return x
+
+# 创建自己的数据集
+class RobotDataset(InMemoryDataset):
     def __init__(self, root, transform=None, pre_transform=None):
-        super(MyDataset, self).__init__(root, transform, pre_transform)
+        super(RobotDataset, self).__init__(root, transform, pre_transform)
         self.data, self.slices = torch.load(self.processed_paths[0])
     @property
     # 数据集加载文件夹
@@ -71,7 +162,8 @@ class MyDataset(InMemoryDataset):
         # 将数据对象加载到属性中
         torch.save((data, slices), self.processed_paths[0])
 
-df = pd.read_csv('data/data_2019-07-03_14-09_ER-Force-vs-TIGERs_Mannheim.txt', 
+# 读取数据
+df = pd.read_csv('data/medium_2019-07-03_14-09_ER-Force-vs-TIGERs_Mannheim.txt', 
     encoding='ISO-8859-1',
     header=None, sep='\t', index_col=False,
     names=['ball_x','ball_y',
@@ -79,27 +171,34 @@ df = pd.read_csv('data/data_2019-07-03_14-09_ER-Force-vs-TIGERs_Mannheim.txt',
     'me6_x','me6_y','me7_x','me7_y','me8_x','me8_y','me9_x','me9_y',
     'enemy1_x','enemy1_y','enemy2_x','enemy2_y','enemy3_x','enemy3_y','enemy4_x',
     'enemy4_y','enemy5_x','enemy5_y','enemy6_x','enemy6_y','enemy7_x','enemy7_y'])
-# 添加序列号
+# 添加序号
 df = df.reset_index()
-print(df)
+# 添加标签
 df.columns=['index',
     'ball_x','ball_y',
     'me1_x','me1_y','me2_x','me2_y','me3_x','me3_y','me4_x','me4_y','me5_x','me5_y',
     'me6_x','me6_y','me7_x','me7_y','me8_x','me8_y','me9_x','me9_y',
     'enemy1_x','enemy1_y','enemy2_x','enemy2_y','enemy3_x','enemy3_y','enemy4_x',
     'enemy4_y','enemy5_x','enemy5_y','enemy6_x','enemy6_y','enemy7_x','enemy7_y']
-# print(df)
 print("----------预处理完成----------")
-dataset = MyDataset(root='../')
-train_dataset = dataset[:2]
-valid_dataset = dataset[2:3]
-test_dataset  = dataset[3:]
+# 构建数据集
+dataset = RobotDataset(root='../')
+# 划分训练集，验证集，测试集
+train_dataset = dataset[:100000]
+valid_dataset = dataset[100000:150000]
+test_dataset  = dataset[150000:]
 print(len(train_dataset), len(valid_dataset), len(test_dataset))
-batch_size = 1
+# 设置单批次样本数
+batch_size = 128
 train_loader = DataLoader(train_dataset, batch_size=batch_size)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
 test_loader  = DataLoader(test_dataset,  batch_size=batch_size)
-# num_items = df.
 print("----------数据集构建完成----------")
+# device = torch.device('cuda')
+# model = Net().to(device)
+# 构造optimizer对象，使用Adam作为优化器，设置学习率0.005
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+# crit = torch.nn.BCELoss()
+print("----------模型训练完成----------")
 
-print("----------模型构建完成----------")
+print("----------误差计算完成----------")
