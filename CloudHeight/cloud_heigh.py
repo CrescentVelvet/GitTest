0bfe40c16b094699d5b7762a51e3da97da5538e9
CrescentVelvet <crescentvelvet3@gmail.com>
2021-03-04 11:43:55 +0800

    添加图像读取函数与校正函数

--- CloudHeight/cloud_heigh.py
+++ CloudHeight/cloud_heigh.py
@@ -1,7 +1,7 @@
 '''
 Author       : velvet
 Date         : 2021-01-09 15:19:57
-LastEditTime : 2021-03-03 22:33:09
+LastEditTime : 2021-03-04 11:43:18
 LastEditors  : velvet
 Description  : 双目视觉云高估计
 '''
@@ -12,24 +12,37 @@ import numpy as np
 import math
 import random
 
+# 图像读取函数
+def read_image(name, type):
+    # 读取彩色图像
+    if type == 1:
+        img = cv2.imread(name, cv2.IMREAD_UNCHANGED)
+    # 读取灰度图像
+    elif type == 0:
+        img = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+    else:
+        print('参数错误')
+    return img
+
 # 图像裁剪函数（圆，R=380）
 def cut_circle(img):
     # 设定裁剪半径
     radius = 380
     # 获取图像尺寸
-    h_img, w_img = img.shape[:2]
-    h_img = round(h_img)
-    w_img = round(w_img)
+    h, w = img.shape[:2]
+    # 四舍五入取整
+    h = round(h)
+    w = round(w)
     # 生成圆形显示模板
-    circleIn = np.zeros((h_img, w_img, 1), np.uint8)
-    circleIn = cv2.circle(circleIn, (h_img//2, w_img//2), radius, (1), -1)
+    circleIn = np.zeros((h, w, 1), np.uint8)
+    circleIn = cv2.circle(circleIn, (h//2, w//2), radius, (1), -1)
     # 生成空白图片
-    img_circle = np.zeros((h_img, w_img, 4), np.uint8)
+    img_circle = np.zeros((h, w, 4), np.uint8)
     # 若为彩色图像
     if (img[1,1]).ndim == 1:
         # 复制前三个通道
-        for y in range(w_img):
-            for x in range(h_img):
+        for y in range(w):
+            for x in range(h):
                 # 原图像矩阵与圆形模板矩阵相乘，得到圆形图像
                 img_circle[x, y, 0] = np.multiply(img[x, y, 0], circleIn[x, y, 0]) # 蓝色通道
                 img_circle[x, y, 1] = np.multiply(img[x, y, 1], circleIn[x, y, 0]) # 绿色通道
@@ -42,8 +55,8 @@ def cut_circle(img):
     # 若为黑白图像
     elif (img[1,1]).ndim == 0:
         # 复制一个通道
-        for y in range(w_img):
-            for x in range(h_img):
+        for y in range(w):
+            for x in range(h):
                 # x为纵坐标，y为横坐标，原点在左上角
                 img_circle[x, y] = np.multiply(img[x, y], circleIn[x, y])
                 # 去除图像中过亮部分
@@ -60,98 +73,73 @@ def image_mosaic(img1,img2):
     return image_merge
 
 # 图像校正函数
-# def image_correct(ratio):
-#     r = 380
-#     # 校正后图片的长宽
-#     w = 3*r
-#     h = 3*r
-#     # 校正中心
-#     x0 = w/2
-#     y0 = h/2
-#     f = 2*r/3.14159
+def image_correct(img):
+    # 原图像半径
+    r = 380
+    # 原图像中心
+    u0 = r
+    v0 = r
+    # 校正图像大小比例
+    cor = 3
+    # 校正图像长宽
+    w = r*cor
+    h = r*cor
+    # 校正图像中心
+    x0 = w/2
+    y0 = h/2
+    # 等距投影焦距
+    f = 2*r/math.pi
+    # 校正图像初始化
+    image_cor = np.zeros((w, h, 4), np.uint8)
+    # 若为彩色图像
+    if (img[1,1]).ndim == 1:
+        # xy为校正图像坐标，uv为原图像坐标
+        for x in range(w):
+            for y in range(h):
+                # 计算校正图像上一点到校正中心的距离
+                h0 = np.sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0))
+                h1 = f*math.atan2(h0,f)
+                if h0 == 0:
+                    u = u0
+                    v = v0
+                else:
+                    u = h1*(x-x0)/h0 + u0
+                    v = h1*(y-y0)/h0 + v0
+                u = round(u)
+                v = round(v)
+                if u>2*u0 or v>2*v0 or u<1 or v<1:
+                    continue
+                image_cor[x, y, 0] = img[u, v, 0] # 蓝色通道
+                image_cor[x, y, 1] = img[u, v, 1] # 绿色通道
+                image_cor[x, y, 2] = img[u, v, 2] # 红色通道
+                image_cor[x, y, 3] = 255        # 透明度通道
+    # 若为黑白图像
+    elif (img[1,1]).ndim == 0:
+        for x in range(w):
+            for y in range(h):
+                # 计算校正图像上一点到校正中心的距离
+                h0 = np.sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0))
+                h1 = f*math.atan2(h0,f)
+                if h0 == 0:
+                    u = u0
+                    v = v0
+                else:
+                    u = h1*(x-x0)/h0 + u0
+                    v = h1*(y-y0)/h0 + v0
+                u = round(u)
+                v = round(v)
+                if u>2*u0 or v>2*v0 or u<1 or v<1:
+                    continue
+                image_cor[x, y] = img[u, v] # 灰度通道
+    else:
+        print("图像类型错误")
+    return image_cor
 
-# 读取彩色图像
-image_sc = cv2.imread('sc.png', cv2.IMREAD_UNCHANGED)
-image_nb = cv2.imread('nb.png', cv2.IMREAD_UNCHANGED)
-
-# 鱼眼畸变校正correct
-# 原图像半径
-r = 380
-# 大视角鱼眼镜头————————————————————————————————————————————
-# # 原图像中心
-# u0 = r
-# v0 = r
-# # 鱼眼镜头半视角
-# xita = math.pi/4
-# # 鱼眼镜头球面半径
-# R = r/math.sin(xita)
-# # 等距投影的距离
-# z = R*3
-# # 校正图像半径
-# r_cor = round(z*math.tan(xita))
-# # 校正图像中心
-# x0 = r_cor
-# y0 = r_cor
-#         # 若为彩色通道
-#         # if (img[1,1]).ndim == 1:
-# # 校正图像初始化
-# image_cor = np.zeros((r_cor*2, r_cor*2, 4), np.uint8)
-# for x in range(r_cor*2):
-#     for y in range(r_cor*2):
-#         # 将xyz坐标系从左上角转换到中心
-#         x = x - x0
-#         y = y - y0
-#         # 用xyz计算uv
-#         h = math.sqrt(x*x + y*y + z*z)
-#         # 将xyz和uv坐标系从中心转换到左上角
-#         u = R*x/h + u0
-#         v = R*y/h + v0
-#         x = x + x0
-#         y = y + y0
-#         # 四舍五入取整
-#         u = round(u)
-#         v = round(v)
-#         # 彩色图像三通道
-#         image_cor[x, y, 0] = image_sc[u, v, 0] # 蓝色通道
-#         image_cor[x, y, 1] = image_sc[u, v, 1] # 绿色通道
-#         image_cor[x, y, 2] = image_sc[u, v, 2] # 红色通道
-# # 透明度通道
-# image_cor[:, :, 3] = 255
-# 基于正交投影————————————————————————————————————————————
-# 校正图像长宽
-w = 3*r
-h = 3*r
-# 校正图像中心
-x0 = w/2
-y0 = h/2
-# 等距投影焦距
-f = 2*r/math.pi
-# 校正图像初始化
-image_cor = np.zeros((w, h, 4), np.uint8)
-for u in range(w):
-    for v in range(h):
-        # 求任意一点到校正中心的距离
-        h0 = np.sqrt((u-x0)*(u-x0)+(v-y0)*(v-y0))
-        h1 = f*math.atan2(h0,f)
-        if h0 == 0:
-            x = r
-            y = r
-        else:
-            x = h1*(u-x0)/h0 + r
-            y = h1*(v-y0)/h0 + r
-        x = round(x)
-        y = round(y)
-        if x>2*r or y>2*r or x<1 or y<1:
-            continue
-        image_cor[u, v, 0] = image_sc[x, y, 0] # 蓝色通道
-        image_cor[u, v, 1] = image_sc[x, y, 1] # 绿色通道
-        image_cor[u, v, 2] = image_sc[x, y, 2] # 红色通道
-        image_cor[u, v, 3] = 255             # 透明度通道
-        image_cor = np.uint8(image_cor)
-# 读取灰度图像
-# image_sc_gray = cv2.imread('sc.png', cv2.IMREAD_GRAYSCALE)
-# image_nb_gray = cv2.imread('nb.png', cv2.IMREAD_GRAYSCALE)
-# image_merge = image_mosaic(image_sc_gray, image_nb_gray)
+image_sc = read_image('sc.png', 1)
+image_nb = read_image('nb.png', 1)
+sc_cor = image_correct(image_sc)
+nb_cor = image_correct(image_nb)
+image_merge = image_mosaic(sc_cor, nb_cor)
 
 # # 图像阈值处理
 # image_sc_thresh = cv2.threshold(image_sc_gray, 30, 255, cv2.THRESH_TOZERO)[1]
@@ -194,4 +182,4 @@ for u in range(w):
 # 输出图像
 # cv2.imwrite('sc_output.png', hmerge1)
 # cv2.imwrite('nb_output.png', hmerge2)
-cv2.imwrite('output.png', image_cor)
+cv2.imwrite('output.png', image_merge)
