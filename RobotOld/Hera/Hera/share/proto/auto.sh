8f00a6cd6cb8b096c7e729e26da2be71056e70ce
CrescentVelvet <crescentvelvet3@gmail.com>
2021-03-06 20:14:50 +0800

    使用最新官方proto文件

--- RobotOld/Hera/Hera/share/proto/auto.sh
+++ RobotOld/Hera/Hera/share/proto/auto.sh
@@ -1,9 +1,2 @@
-if [ ! -d cpp ]; then
-	mkdir cpp
-fi
-protoc --cpp_out=cpp *.proto
-# if [ ! -f python ]; then
-# 	mkdir python
-# fi
-# protoc --python_out=python --cpp_out=cpp *.proto
-# touch python/__init__.py
+protoc --python_out=python --cpp_out=cpp *.proto
+touch python/__init__.py
